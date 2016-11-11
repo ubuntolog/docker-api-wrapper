73dcb53d71a3c78883d9d018f48e6f78a510f8bc
ubuntolog <kstchernov@gmail.com>
2016-11-11 15:53:27 +0100

    Connection to API works

--- src/github.com/ubuntolog/docker-api-wrapper/main.go
+++ src/github.com/ubuntolog/docker-api-wrapper/main.go
@@ -1,1 +1,49 @@
-package docker_api_wrapper
+package main
+
+import (
+	"fmt"
+	//"io"
+	"net"
+	"net/http"
+	"os"
+	"io"
+)
+
+func dieIf(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v", err)
+		os.Exit(1)
+	}
+}
+
+const sock = "/var/run/docker.sock"
+
+func server() {
+	l, err := net.Listen("unix", sock)
+	dieIf(err)
+	srv := &http.Server{
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "hello: %v\n", r.RequestURI)
+		}),
+	}
+	srv.Serve(l)
+}
+
+func fakeDial(proto, addr string) (conn net.Conn, err error) {
+	return net.Dial("unix", sock)
+}
+
+func main() {
+	defer os.Remove(sock)
+	//go server()
+
+	tr := &http.Transport{
+		Dial: fakeDial,
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Get("http://localhost:4243/containers/json")
+	dieIf(err)
+	io.Copy(os.Stdout, resp.Body)
+	resp.Body.Close()
+
+}
